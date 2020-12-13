4208ba3d6c43741c5037c9c6cb01210ca33d0cea
Brennan Newton <brennanmnewton@gmail.com>
2020-12-13 14:03:31 -0500

    Set up middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,13 @@ import express from 'express';
 import {
   indexPage, aboutPage, messagesPage, addMessage
 } from '../controllers';
+import { modifyMessage } from '../middleware';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/about', aboutPage);
 indexRouter.get('/messages', messagesPage);
-indexRouter.post('/messages', addMessage);
+indexRouter.post('/messages', modifyMessage, addMessage);
 
 export default indexRouter;
